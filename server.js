a3d3d33a18e911164398a1c567ccd81876f5bcb5
artraxus <artraxus@gmail.com>
2014-01-02 11:57:29 +0100

    another tentative

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var app = express();
 
-var ipaddr = process.env.OPENSHIFT_INTERNAL_IP || "127.0.0.1";
-var port = process.env.OPENSHIFT_INTERNAL_PORT || 8080;
+var port = process.env.OPENSHIFT_NODEJS_PORT || process.env.OPENSHIFT_INTERNAL_PORT || 8080;
+var ipaddr = process.env.OPENSHIFT_NODEJS_IP || process.env.OPENSHIFT_INTERNAL_IP || "127.0.0.1";
+
+console.log(port);
+console.log(ipaddr);
 
 app.configure(function () {
     app.use(express.bodyParser());
